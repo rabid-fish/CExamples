5451ccdd8487ca010a2096d80da9763624979152
rabid-fish <spam@danandlaurajuliano.com>
2013-03-07 08:50:26 -0600

    Adding subdir and library examples

---
+++ MakeExample5/cleanup.sh
@@ -0,0 +1,8 @@
+rm aclocal.m4
+rm -rf autom4*
+rm configure
+rm config.log
+rm config.status
+rm *.*~
+rm Makefile
+rm Makefile.in
---
+++ MakeExample6/cleanup.sh
@@ -0,0 +1,10 @@
+rm aclocal.m4
+rm -rf autom4*
+rm configure
+rm config.log
+rm config.status
+rm *.*~
+rm Makefile
+rm Makefile.in
+rm -rf .deps
+rm -rf build-aux/*
